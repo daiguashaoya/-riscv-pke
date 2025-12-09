47fe5970934d8dc42b5adb8fa1b4e7e21709ec49
ltp <3035801653@qq.com>
2025-12-09 10:05:25 +0800

    finish lab2_3

--- kernel/strap.c
+++ kernel/strap.c
@@ -9,6 +9,7 @@
 #include "pmm.h"
 #include "vmm.h"
 #include "util/functions.h"
+#include "memlayout.h"
 
 
 #include "spike_interface/spike_utils.h"
@@ -59,7 +60,18 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
       // dynamically increase application stack.
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
-      panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+      if(stval<USER_STACK_TOP&&stval>=USER_STACK_TOP-20*PGSIZE)
+      {
+        void * pa=alloc_page();
+        if(pa==0)
+          panic("Cant alloc more pages!");
+        // 设置为页尾的地址
+        uint64 va=stval-(stval%PGSIZE);
+        user_vm_map((pagetable_t)current->pagetable, va, PGSIZE, (uint64)pa,
+                      prot_to_type(PROT_WRITE | PROT_READ, 1));
+      }
+      else
+        panic("Invalid address acess!");
 
       break;
     default:
