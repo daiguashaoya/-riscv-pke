a217cc22dab04cd81de0acf78b55f70ba37bf340
ltp <3035801653@qq.com>
2025-12-18 15:31:52 +0800

    finish lab2_challenge1

--- kernel/strap.c
+++ kernel/strap.c
@@ -56,22 +56,33 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
   sprint("handle_page_fault: %lx\n", stval);
   switch (mcause) {
     case CAUSE_STORE_PAGE_FAULT:
+    case CAUSE_LOAD_PAGE_FAULT:
       // TODO (lab2_3): implement the operations that solve the page fault to
       // dynamically increase application stack.
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
-      if(stval<USER_STACK_TOP&&stval>=USER_STACK_TOP-20*PGSIZE)
-      {
-        void * pa=alloc_page();
-        if(pa==0)
+    if(stval < USER_STACK_TOP && stval >= USER_STACK_TOP - 20 * PGSIZE) {
+        // 合法的栈扩展请求：分配物理页并映射
+        void * pa = alloc_page();
+        if(pa == 0)
           panic("Cant alloc more pages!");
-        // 设置为页尾的地址
-        uint64 va=stval-(stval%PGSIZE);
+        
+        // 对齐到页面边界
+        uint64 va = stval - (stval % PGSIZE);
+        
+        // 建立映射
         user_vm_map((pagetable_t)current->pagetable, va, PGSIZE, (uint64)pa,
-                      prot_to_type(PROT_WRITE | PROT_READ, 1));
+                    prot_to_type(PROT_WRITE | PROT_READ, 1));
       }
-      else
-        panic("Invalid address acess!");
+      else {
+        // 2. 处理非法地址访问 (lab2_challenge1)
+        // 不在栈范围内，视为非法访问 (如堆越界)
+        sprint("this address is not available!\n");
+        
+        // 退出程序 (使用 shutdown 模拟进程退出)
+        // 注意：这里不能用 panic，因为 panic 代表内核崩溃，而这里是用户程序错误
+        shutdown(-1);
+      }
 
       break;
     default:
