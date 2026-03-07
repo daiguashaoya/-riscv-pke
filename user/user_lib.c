9c007aa2f529aff9f24ea45a7bc4d8da4fe43ec1
ltp <3035801653@qq.com>
2026-03-07 22:18:28 +0800

    备份

--- user/user_lib.c
+++ user/user_lib.c
@@ -1,27 +1,26 @@
 /*
  * The supporting library for applications.
- * Actually, supporting routines for applications are catalogued as the user 
- * library. we don't do that in PKE to make the relationship between application 
+ * Actually, supporting routines for applications are catalogued as the user
+ * library. we don't do that in PKE to make the relationship between application
  * and user library more straightforward.
  */
 
 #include "user_lib.h"
-#include "util/types.h"
-#include "util/snprintf.h"
 #include "kernel/syscall.h"
+#include "util/snprintf.h"
+#include "util/types.h"
 
-uint64 do_user_call(uint64 sysnum, uint64 a1, uint64 a2, uint64 a3, uint64 a4, uint64 a5, uint64 a6,
-                 uint64 a7) {
+uint64 do_user_call(uint64 sysnum, uint64 a1, uint64 a2, uint64 a3, uint64 a4,
+                    uint64 a5, uint64 a6, uint64 a7) {
   int ret;
 
-  // before invoking the syscall, arguments of do_user_call are already loaded into the argument
-  // registers (a0-a7) of our (emulated) risc-v machine.
-  asm volatile(
-      "ecall\n"
-      "sw a0, %0"  // returns a 32-bit value
-      : "=m"(ret)
-      :
-      : "memory");
+  // before invoking the syscall, arguments of do_user_call are already loaded
+  // into the argument registers (a0-a7) of our (emulated) risc-v machine.
+  asm volatile("ecall\n"
+               "sw a0, %0" // returns a 32-bit value
+               : "=m"(ret)
+               :
+               : "memory");
 
   return ret;
 }
@@ -29,14 +28,14 @@ uint64 do_user_call(uint64 sysnum, uint64 a1, uint64 a2, uint64 a3, uint64 a4, u
 //
 // printu() supports user/lab1_1_helloworld.c
 //
-int printu(const char* s, ...) {
+int printu(const char *s, ...) {
   va_list vl;
   va_start(vl, s);
 
-  char out[256];  // fixed buffer size.
+  char out[256]; // fixed buffer size.
   int res = vsnprintf(out, sizeof(out), s, vl);
   va_end(vl);
-  const char* buf = out;
+  const char *buf = out;
   size_t n = res < sizeof(out) ? res : sizeof(out);
 
   // make a syscall to implement the required functionality.
@@ -47,35 +46,31 @@ int printu(const char* s, ...) {
 // applications need to call exit to quit execution.
 //
 int exit(int code) {
-  return do_user_call(SYS_user_exit, code, 0, 0, 0, 0, 0, 0); 
+  return do_user_call(SYS_user_exit, code, 0, 0, 0, 0, 0, 0);
 }
 
 //
 // lib call to naive_malloc
 //
-void* naive_malloc() {
-  return (void*)do_user_call(SYS_user_allocate_page, 0, 0, 0, 0, 0, 0, 0);
+void *naive_malloc() {
+  return (void *)do_user_call(SYS_user_allocate_page, 0, 0, 0, 0, 0, 0, 0);
 }
 
 //
 // lib call to naive_free
 //
-void naive_free(void* va) {
+void naive_free(void *va) {
   do_user_call(SYS_user_free_page, (uint64)va, 0, 0, 0, 0, 0, 0);
 }
 
 //
 // lib call to naive_fork
-int fork() {
-  return do_user_call(SYS_user_fork, 0, 0, 0, 0, 0, 0, 0);
-}
+int fork() { return do_user_call(SYS_user_fork, 0, 0, 0, 0, 0, 0, 0); }
 
 //
 // lib call to yield
 //
-void yield() {
-  do_user_call(SYS_user_yield, 0, 0, 0, 0, 0, 0, 0);
-}
+void yield() { do_user_call(SYS_user_yield, 0, 0, 0, 0, 0, 0, 0); }
 
 //
 // lib call to open
@@ -87,7 +82,7 @@ int open(const char *pathname, int flags) {
 //
 // lib call to read
 //
-int read_u(int fd, void * buf, uint64 count){
+int read_u(int fd, void *buf, uint64 count) {
   return do_user_call(SYS_user_read, fd, (uint64)buf, count, 0, 0, 0, 0);
 }
 
@@ -100,7 +95,7 @@ int write_u(int fd, void *buf, uint64 count) {
 
 //
 // lib call to seek
-// 
+//
 int lseek_u(int fd, int offset, int whence) {
   return do_user_call(SYS_user_lseek, fd, offset, whence, 0, 0, 0, 0);
 }
@@ -145,25 +140,30 @@ int mkdir_u(const char *pathname) {
 //
 int closedir_u(int fd) {
   return do_user_call(SYS_user_closedir, fd, 0, 0, 0, 0, 0, 0);
-} 
+}
 
 //
 // lib call to link
 //
-int link_u(const char *fn1, const char *fn2){
+int link_u(const char *fn1, const char *fn2) {
   return do_user_call(SYS_user_link, (uint64)fn1, (uint64)fn2, 0, 0, 0, 0, 0);
 }
 
 //
 // lib call to unlink
 //
-int unlink_u(const char *fn){
+int unlink_u(const char *fn) {
   return do_user_call(SYS_user_unlink, (uint64)fn, 0, 0, 0, 0, 0, 0);
 }
 
 //
 // lib call to close
 //
-int close(int fd) {
-  return do_user_call(SYS_user_close, fd, 0, 0, 0, 0, 0, 0);
+int close(int fd) { return do_user_call(SYS_user_close, fd, 0, 0, 0, 0, 0, 0); }
+
+// added @ lab4_challenge3
+int wait(int pid) { return do_user_call(SYS_user_wait, pid, 0, 0, 0, 0, 0, 0); }
+
+int exec(char *path, char *para) {
+  return do_user_call(SYS_user_exec, (uint64)path, (uint64)para, 0, 0, 0, 0, 0);
 }
