bb879f18ebf40cb26a38136344a434ca38271c95
ltp <3035801653@qq.com>
2026-03-11 17:54:41 +0800

    除了多核逻辑 其余全部成功合并

--- run_all_app.sh
+++ run_all_app.sh
@@ -1,14 +1,44 @@
-make clean;make
+#!/bin/bash
+make clean
+make
+
+echo
+echo -e "\033[1;34m>>> spike ./obj/riscv-pke bin/app_print_backtrace\033[0m"
 spike ./obj/riscv-pke bin/app_print_backtrace
+echo
+
+echo -e "\033[1;34m>>> spike ./obj/riscv-pke bin/app_errorline\033[0m"
 spike ./obj/riscv-pke bin/app_errorline
+echo
 
+echo -e "\033[1;34m>>> spike ./obj/riscv-pke bin/app_sum_sequence\033[0m"
 spike ./obj/riscv-pke bin/app_sum_sequence
+echo
+
+echo -e "\033[1;34m>>> spike ./obj/riscv-pke bin/app_singlepageheap\033[0m"
 spike ./obj/riscv-pke bin/app_singlepageheap
+echo
 
+echo -e "\033[1;34m>>> spike ./obj/riscv-pke bin/app_wait\033[0m"
 spike ./obj/riscv-pke bin/app_wait
-spike ./obj/riscv-pke bin/app_semaphore
-spike ./obj/riscv-pke bin/app_cow
+echo
 
+echo -e "\033[1;34m>>> spike ./obj/riscv-pke bin/app_semaphore\033[0m"
+spike ./obj/riscv-pke bin/app_semaphore
+echo
+
+echo -e "\033[1;34m>>> spike ./obj/riscv-pke bin/app_cow\033[0m"
+spike ./obj/riscv-pke bin/app_cow
+echo
+
+echo -e "\033[1;34m>>> spike ./obj/riscv-pke bin/app_relativepath\033[0m"
 spike ./obj/riscv-pke bin/app_relativepath
+echo
+
+echo -e "\033[1;34m>>> spike ./obj/riscv-pke bin/app_exec\033[0m"
 spike ./obj/riscv-pke bin/app_exec
+echo
+
+echo -e "\033[1;34m>>> spike ./obj/riscv-pke bin/app_shell\033[0m"
 spike ./obj/riscv-pke bin/app_shell
+echo
